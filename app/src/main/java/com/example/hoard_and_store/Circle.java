29bd01326e702618a9fdf4c8962bc6e9808888f9
PhamNamSon <phamnamson1999@gmail.com>
2023-10-09 14:12:30 -0600

    Add Circle class

--- app/src/main/java/com/example/hoard_and_store/Circle.java
+++ app/src/main/java/com/example/hoard_and_store/Circle.java
@@ -2,4 +2,10 @@ package com.example.hoard_and_store;
 
 public class Circle extends Shape {
 
+    public Circle(int x, int y) {
+        super(x, y);
+    }
+    @Override
+    public void draw() {
+    }
 }
