c4fa2a7d89615bb7f0ebdda88e4f09b21ca031ec
paudeldeepak <deepak_paudel@icloud.com>
2023-10-09 21:36:37 -0600

    fixed two colors

--- app/src/main/java/com/example/hoard_and_store/Shape.java
+++ app/src/main/java/com/example/hoard_and_store/Shape.java
@@ -5,8 +5,6 @@ public abstract class Shape {
     protected int y;
     String color = "red";
 
-    String color = "blue";
-
     public Shape(int x, int y) {
         this.x = x;
         this.y = y;
