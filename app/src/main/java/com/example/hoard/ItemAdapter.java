0dbb70a41027ad144ff2ef39cfc05dea628cb71e
imgon <kosedy@ualberta.ca>
2023-11-09 23:16:56 -0700

    Only do single sorting like said by TA

--- app/src/main/java/com/example/hoard/ItemAdapter.java
+++ app/src/main/java/com/example/hoard/ItemAdapter.java
@@ -329,7 +329,7 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ViewHolder> im
         }
 
         void bind(boolean isSelected) {
-            itemView.setBackgroundColor(isSelected ? Color.LTGRAY : Color.WHITE);
+            itemView.setBackgroundColor(isSelected ? Color.LTGRAY : Color.TRANSPARENT);
         }
     }
 
