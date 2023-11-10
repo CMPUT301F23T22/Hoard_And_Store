0f6b8dd487e0450dfed599e5b4bd049fd54f3786
koss756 <lkosedy24@gmail.com>
2023-11-10 15:54:25 -0700

    Added some UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,13 +35,17 @@ android {
 dependencies {
     implementation("com.github.skydoves:colorpickerview:2.3.0")
     implementation(platform("com.google.firebase:firebase-bom:32.4.0"))
-    implementation("com.google.firebase:firebase-firestore")
+    implementation("com.google.firebase:firebase-firestore:24.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.espresso:espresso-contrib:3.5.1") {
+        exclude("com.google.protobuf", "protobuf-lite")
+    }
     implementation("com.google.android.material:material:1.3.0-alpha03")
     implementation ("com.github.skydoves:colorpickerview:2.3.0")
+
 }
